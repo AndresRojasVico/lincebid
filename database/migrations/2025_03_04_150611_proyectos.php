0b1f23422e5651cf6940e162285b2fa433b52c7c
andres <rojasvico@hotmail.com>
2025-03-05 10:02:47 +0100

    migracion de estados proyectos ok

--- database/migrations/2025_03_04_150611_proyectos.php
+++ database/migrations/2025_03_04_150611_proyectos.php
@@ -59,5 +59,6 @@ return new class extends Migration
     public function down(): void
     {
         //
+        Schema::dropIfExists('proyectos');
     }
 };
---
+++ database/migrations/2025_03_05_081653_estados_proyectos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+
+        
+        DB::statement("
+        CREATE TABLE estados_proyectos(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            estado VARCHAR(100)
+        );
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('estados_proyectos');
+    }
+};
