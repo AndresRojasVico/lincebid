27263a545f0bba9ed6df750991ed73e63508c5a1
Andres <rojasvico@hotmail.com>
2025-03-24 07:19:18 +0100

    con al actualizacion de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
-class UserController extends  Controller
+class UserController extends Controller
 {
+    use ValidatesRequests;
     //
 
     public function config()
@@ -15,13 +17,26 @@ class UserController extends  Controller
     }
     public function update(Request $request)
     {
+        // Conseguir usuario identificado
+        $user = Auth::user();
+
+        // Validación del formulario
+        $validate = $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . Auth::user()->id],
+        ]);
+        //recoger datos del formulario
         $id = Auth::user()->id;
         $name = $request->input('name');
+        $surname = $request->input('surname');
         $email = $request->input('email');
-
-        var_dump($name);
-        var_dump($email);
-        var_dump($id);
-        die();
+        //asignar nuevos valores al objeto del usuario
+        $user->name = $name;
+        $user->surname = $surname;
+        $user->email = $email;
+        //Ejecutar consulta y cambios en la base de datos
+        $user->save();
+        return redirect()->route('config')->with(['message' => 'Usuario actualizado correctamente']);
     }
 }
