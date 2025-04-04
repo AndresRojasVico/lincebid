c1cd6f68942093b060789d117b8642e36136282f
Andres <rojasvico@hotmail.com>
2025-04-04 07:21:13 +0200

    enlaces de

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+
                     <h3>hola {{ Auth::user()->name }}</h3>
 
-                    <div class="tex_rojo">esto tendria que ser rojo</div>
+                    @if (Auth::user()->isAdmin())
+                    <a href="{{ route('user.admin') }}">Zona de administrador</a>
+                    @else
+                    <h3>User</h3>
+                    @endif
+
+
 
                 </div>
             </div>
